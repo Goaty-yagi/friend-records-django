1f6093fd332a7024f837f16a3550fdf757081b1e
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-06-27 14:20:01 +1000

    done events all endpoint test.

--- events/views.py
+++ events/views.py
@@ -11,20 +11,20 @@ from .models import Event
 from friends.models import Friend
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-class OwnerOnlyRestriction(UserPassesTestMixin):
-    '''
-    restriction of owner user can access the only event data
-    '''
-    def test_func(self):
-        print('in_testfun')
-        owner_user = self.request.user
-        object_user = Friend.objects.get(id=self.get_object().friend_id).user
-        print("owner",owner_user, "object",object_user)
-        verification = True if owner_user.is_staff or owner_user==object_user else False
-        return verification
+# class OwnerOnlyRestriction(UserPassesTestMixin):
+#     '''
+#     restriction of owner user can access the only event data
+#     '''
+#     def test_func(self):
+#         print('in_testfun',self.request.__dir__())
+#         owner_user = self.request.user
+#         object_user = Friend.objects.get(id=self.get_object().friend_id).user
+#         print("owner",owner_user, "object",object_user)
+#         verification = True if owner_user.is_staff or owner_user==object_user else False
+#         return verification
     
-    def handle_no_permission(self):
-        return JsonResponse({"info":"not allowed"},status=403)
+#     def handle_no_permission(self):
+#         return JsonResponse({"info":"not allowed"},status=403)
 
 class EventCreateApi(generics.CreateAPIView):
     serializer_class = EventCreateSerializer
@@ -39,13 +39,13 @@ class EventListApi(generics.ListAPIView):
 class EventUserListApi(APIView):
     # get characterevent list.
     def post(self, request):
-        character_uid = request.data.pop('character')
-        character_event = Event.objects.filter(character=character_uid)
+        friend_id = request.data.pop('friend')
+        character_event = Event.objects.filter(friend=friend_id)
         serializer = EventCreateSerializer(character_event, many=True)
         return Response(serializer.data)
 
 
-class EventDetailApi(OwnerOnlyRestriction, generics.RetrieveUpdateDestroyAPIView):
+class EventDetailApi(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventCreateSerializer
     queryset = Event.objects.all()
     lookup_field = 'id'
