379a5125c990b9b5a1cdb4fe895003a196b7a765
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-09-07 23:00:43 +1000

    test3

--- root/settings.py
+++ root/settings.py
@@ -73,13 +73,13 @@ MIDDLEWARE = [
 
 # corsheaders setting start
 
-# CORS_ALLOWED_ORIGINS = getenv(
-#     'CORS_ALLOWED_ORIGINS',
-#     'http://localhost:3000,http://127.0.0.1:3000'
-# ).split(',')
+CORS_ALLOWED_ORIGINS = getenv(
+    'CORS_ALLOWED_ORIGINS',
+    'http://localhost:3000,http://127.0.0.1:3000'
+).split(',')
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_HEADERS = "*"
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 # corsheaders setting end
 
 ROOT_URLCONF = 'root.urls'
