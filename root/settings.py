217f029d8f84d3f5d26f455bfe80bd55b488d4ff
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-09-07 19:10:58 +1000

    add static settings

--- root/settings.py
+++ root/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from os import getenv, path
 from pathlib import Path
 import dotenv
@@ -151,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
